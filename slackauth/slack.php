bc2a704de3d2f0a5d1b000a9114579414428aee6
VTHEVMU <muthukumar.thevar@pearson.com>
2017-04-10 15:33:40 +0530

    Debugging 2

--- slackauth/slack.php
+++ slackauth/slack.php
@@ -5,12 +5,12 @@
         curl_setopt($curl, CURLOPT_URL, 'https://slack.com/api/oauth.access?code='.$_GET['code'].'&client_id=112655623728.166099875590&client_secret=8a7c321e7b5d5f53ab544298d9b79c07');
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $resp = curl_exec($curl);
+        curl_close($curl);
         $objectResp = json_decode($resp);
         $array = json_decode(json_encode($objectResp), true);
-        $token= $array["token"];
-        curl_close($curl);
-        echo $token;
+        print_r($array);
         exit;
+        $token= $array["token"];
         curl_setopt($curl, CURLOPT_URL, "https://slack.com/api/users.list?presence=true&code=$token&pretty=1");
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $resp = curl_exec($curl);
