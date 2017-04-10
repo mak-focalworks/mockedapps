7ed24600876ca90e7492a88161431e102130879d
VTHEVMU <muthukumar.thevar@pearson.com>
2017-04-10 15:50:05 +0530

    Debugging 4

--- slackauth/slack.php
+++ slackauth/slack.php
@@ -1,14 +1,16 @@
 <?php
     if(isset($_GET['code'])){
         //echo 'http://slack.com/api/oauth.access?code='.$_GET['code'].'&client_id=112655623728.166099875590&client_secret=2f9422684fe95fcc9733fd8429b13699';
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, 'https://slack.com/api/oauth.access?code='.$_GET['code'].'&client_id=112655623728.166099875590&client_secret=8a7c321e7b5d5f53ab544298d9b79c07');
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $resp = curl_exec($curl);
+        $curl1 = curl_init();
+        curl_setopt($curl1, CURLOPT_URL, 'https://slack.com/api/oauth.access?code='.$_GET['code'].'&client_id=112655623728.166099875590&client_secret=8a7c321e7b5d5f53ab544298d9b79c07');
+        curl_setopt($curl1, CURLOPT_RETURNTRANSFER, true);
+        $resp = curl_exec($curl1);
         $objectResp = json_decode($resp);
         $array = json_decode(json_encode($objectResp), true);
         $token= $array["access_token"];
-        echo $token;
+        curl_close($curl1);
+        //---------------------------------------------
+        $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, "https://slack.com/api/users.list?presence=true&code=$token&pretty=1");
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $resp = curl_exec($curl);
